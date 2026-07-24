c93368d84c6885d67a9a12c5f2e4e9d4e285934f
Edwinta-dev <edwintankj@gmail.com>
2026-07-24 23:02:10 +0800

    Importing hsvEngine and imagSchedule for state management. Configuration of http response to esp32

--- Backend/SensorAPI/camera/camera.py
+++ Backend/SensorAPI/camera/camera.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify
 from flask_cors import CORS 
 from supabase import create_client, Client
-
+import hsvEngine
+import imageSchedule
 
 load_dotenv()
 
@@ -27,7 +28,13 @@ def upload_image():
 
     filename = f"wifi_photo_{int(time.time())}.jpg"
     bucket_name = 'imageAnalysisBucket'
-
+    state = hsvEngine.getstate()
+    if state == "base":
+        time_to_next_image = imageSchedule.get_base_schedule_sleep_seconds()
+    elif state == "dynamic":
+        time_to_next_image = imageSchedule.get_dynamicstate_sleep_seconds()
+    elif state == "obstruction":
+        time_to_next_image = imageSchedule.get_obstructionstate_sleep_seconds()
     try:
         # 2. Upload file bytes to Supabase Storage with explicit MIME type
         response = supabase.storage.from_(bucket_name).upload(
@@ -36,8 +43,12 @@ def upload_image():
             file_options={"content-type": "image/jpeg", "upsert": "true"}
         )
         
-        print(f"Uploaded {filename} ({len(file_bytes)} bytes)")
-        return "Image written successfully!", 200
+        print(f"Uploaded {filename} ({len(file_bytes)} bytes)")        
+        # Http Response
+        return jsonify({
+            "status": "success",
+            "sleep_sec": time_to_next_image
+        }), 200
 
     except Exception as e:
         print(f"Supabase upload failed: {str(e)}")
